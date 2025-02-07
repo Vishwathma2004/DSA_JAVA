8aefd16ad1b84cee4f28ae827f9ca0137a3dfeea
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-02-07 16:03:18 +0530

    recursion

--- patternQuestion/pattern8.java
+++ patternQuestion/pattern8.java
@@ -3,10 +3,11 @@ public class pattern8{
         pattern88(5);
     }
     static void pattern88(int n){
+        int OriginalN = n;
         n=2*n;
         for(int row=0;row<=n;row++){
             for(int col=0;col<n;col++){
-                int atEveryIndex = Math.min((Math.min(row,col)),Math.min(n-row,n-col));
+                int atEveryIndex = OriginalN- Math.min((Math.min(row,col)),Math.min(n-row,n-col));
                 System.out.print(atEveryIndex+" ");
             }
             System.out.println();
---
+++ recursion/hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main(String[] args) {
+        message();
+    }
+    static void message(){
+        System.out.println("Hello World");
+    }
+}
