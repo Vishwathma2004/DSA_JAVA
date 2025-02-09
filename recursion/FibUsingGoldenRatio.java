c0ee4a31779c0410c0e845716b14c6177c2b58c5
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-02-09 08:13:30 +0530

    recursion

---
+++ recursion/FibUsingGoldenRatio.java
@@ -0,0 +1,13 @@
+public class FibUsingGoldenRatio {
+    public static void main(String[] args) {
+        // Loop through the first 11 Fibonacci numbers
+        for (int i = 0; i < 11; i++) {
+            System.out.println(fibFormula(i)); // Pass the index i as the parameter
+        }
+    }
+
+    static int fibFormula(int n) {
+        // Using the Golden Ratio (Binet's formula)
+        return (int)Math.round((Math.pow(((1 + Math.sqrt(5)) / 2), n) / Math.sqrt(5))); 
+    }
+}
