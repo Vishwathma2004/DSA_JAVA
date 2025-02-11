d09b6c68d4e05e126c56fb9307a9c30ec58123bc
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-02-11 15:02:19 +0530

    recursion

--- recursion/mergeSortInPlace.java
+++ recursion/mergeSortInPlace.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class MergeSortInPlace {
+public class mergeSortInPlace {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements: ");
