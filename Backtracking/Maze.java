025cd1f464356f1a3708791aab29f6183f3ccb20
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-02-13 20:12:16 +0530

    DSA

---
+++ Backtracking/Maze.java
@@ -0,0 +1,13 @@
+public class Maze{
+    public static void main(String[] args) {
+        System.out.println(count(2, 2));
+    }
+    static int count(int r,int c){
+        if(r==1||c==1){
+            return 1;
+        }
+        int left = count(r-1,c);
+        int right = count(r,c-1);
+        return left+right;
+    }
+}
