e8ab26290d9d7fd91f9471effee4fb568b7f222a
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-02-22 20:55:59 +0530

    Abstraction

---
+++ OOPS/Abstraction/Main.java
@@ -0,0 +1,9 @@
+package Abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+        getter_and_setter obj = new getter_and_setter();
+        obj.setName("Alice");
+        System.out.println("Name : "+obj.getName());
+    }
+}
---
+++ OOPS/Abstraction/getter_and_setter.java
@@ -0,0 +1,11 @@
+package Abstraction;
+
+public class getter_and_setter {
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
