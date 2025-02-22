47cc739ebebadf92f39f1746ca66a8b7cde43870
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-02-22 20:55:34 +0530

    Polymorphism

--- OOPS/Polymorphism/Circle.java
+++ OOPS/Polymorphism/Circle.java
@@ -1,7 +1,12 @@
 package Polymorphism;
 
 public class Circle extends Shapes {
-    void shape(){
+
+    //this will run when obj of circle is created
+    //hence it is overriding the parent method
+    
+    @Override
+    void area(){
         System.out.println("Area is pie*r*r");
     }
 }
