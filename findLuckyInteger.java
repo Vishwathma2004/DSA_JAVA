d82a3fab886f5eeb2fe0fcb3bda91811575a9a57
Vishwathma N <22c58.vishwathma@sjec.ac.in>
2025-03-01 21:17:16 +0530

    Leetcode 1394

---
+++ findLuckyInteger.java
@@ -0,0 +1,32 @@
+package Leetcode;
+
+import java.util.Scanner;
+
+
+public class findLuckyInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The Lucky Integer is");
+        System.out.println(luckyInteger(arr));
+    }
+    public static int luckyInteger(int[] arr){
+        int[] freq = new int[501];
+        for(int i=0;i<arr.length;i++){
+            freq[arr[i]]++;
+        }
+        int ans = -1;
+        for (int i = 1; i < freq.length; i++) {
+            if(freq[i]==i){
+                ans = i;
+            }
+        }
+        return ans;
+    }
+}
